6e37309d3da52750b4942ad58cb41869c340fc08
Tushar Samant <tushar@tmetic.com>
2018-04-07 17:13:01 -0500

    Add build command for watch.c

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 clang -o watchfile watchfile.c
 clang -o watchdir watchdir.c     -framework CoreServices
+clang -o watch watch.c           -framework CoreServices
 clang -o watchpower watchpower.c -framework IOKit -framework CoreFoundation
 clang -o watchusb watchusb.c     -framework IOKit -framework CoreFoundation 
